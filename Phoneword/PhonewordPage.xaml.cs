ac6739e1212d67ed1573867d65dc53d6cea2fe12
Watanabe Yuichi <nabe.gm@gmail.com>
2016-11-19 16:35:17 +0900

    create

--- Phoneword/PhonewordPage.xaml.cs
+++ Phoneword/PhonewordPage.xaml.cs
@@ -1,12 +1,45 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Phoneword
 {
 	public partial class PhonewordPage : ContentPage
 	{
+		string translatedNumber;
+
 		public PhonewordPage()
 		{
 			InitializeComponent();
+		}
+
+		void OnTranslate(object sender, EventArgs e)
+		{
+			translatedNumber = Phoneword.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+			if (!string.IsNullOrWhiteSpace(translatedNumber))
+			{
+				callButton.IsEnabled = true;
+				callButton.Text = "Call " + translatedNumber;
+			}
+			else {
+				callButton.IsEnabled = false;
+				callButton.Text = "Call";
+			}
+		}
+
+		async void OnCall(object sender, EventArgs e)
+		{
+			if (await this.DisplayAlert(
+					"Dial a Number",
+					"Would you like to call " + translatedNumber + "?",
+					"Yes",
+					"No"))
+			{
+				//var dialer = DependencyService.Get<IDialer>();
+				//if (dialer != null)
+				//	dialer.Dial(translatedNumber);
+				Device.OpenUri(new Uri($"tel:{translatedNumber}"));
+			}
 		}
 	}
 }
